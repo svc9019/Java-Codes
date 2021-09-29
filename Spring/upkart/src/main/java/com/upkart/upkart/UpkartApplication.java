3d1750bf1877bd3cfeeba8b9cc43e552c661529f
sudosip <80571800+sudosip@users.noreply.github.com>
2021-09-29 21:31:33 +0530

    Upkart Project

---
+++ Spring/upkart/src/main/java/com/upkart/upkart/UpkartApplication.java
@@ -0,0 +1,13 @@
+package com.upkart.upkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpkartApplication.class, args);
+	}
+
+}
---
+++ Spring/upkart/src/test/java/com/upkart/upkart/UpkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upkart.upkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
